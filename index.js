b2cd3c47e85459b117e699a3c76762ba01c9ace9
JEI <jei@genus.no>
2017-06-26 12:06:56 +0200

    added checking of branch

---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+const request = require('request')
+const program = require('commander')
+const fs      = require('fs')
+
+
+const getVersionNumber = require('./functions/versionService').getVersionNumber
+const checkBranch      = require('./functions/versionService').checkBranch
+
+
+program
+    .version('0.0.1')
+    .option('-b, --branch [branch]', 'Git branch')
+    .option('-d, --destination [path]', 'Destination')
+
+
+program
+    .command('get')
+    .description('Gets the version number')
+    .action(function(cmd, options) {
+        console.log("Gets the version number")
+
+        let branch = program.branch || process.env.CI_COMMIT_REF_NAME
+
+        checkBranch(branch)
+            .then(getVersionNumber)
+            .then(function(version){
+            
+                if (program.destination) {
+                    fs.writeFile(program.destination, version, function(err){
+                        if (err){
+                            throw err
+                        }
+                        console.log("Versionnumber saved in " + program.destination )
+                    })
+                } else {
+
+                    console.log(version)
+                }
+            })
+            .catch(function(err) {
+                console.error("ERROR: " + err)
+            })
+    })
+
+
+
+program.parse(process.argv)
